d8be0e087240d2c6d640f49317197833a833cf61
Iwo <iwoskater3@hotmail.com>
2022-04-04 18:43:59 +0200

    previous changes applied to new repo

---
+++ www/js/init.js
@@ -0,0 +1,41 @@
+(function($){
+  $(function(){
+
+    //$('.parallax').parallax();
+    $('select').formSelect();
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+document.addEventListener('deviceready', onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+ 
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    //LoginButton onclick function
+    $("#loginButton").click(function(){
+      let pin = $("#pin").val();
+      let pinList = [1234,1111];
+      let sToken;
+      //We identify the API to be used
+      let usedAPI;
+      let isValid= false;
+      //Pin check
+      for(let i = 0;i<pinList.length;i++){
+        if (pinList[i]==pin){
+          isValid=true;
+        }
+      }
+      if(isValid){
+        window.location.assign('emulator.html');
+      }else{
+        alert("Error: Introdueix el pin correcte");
+      }
+      
+      
+      //Page reload prevention
+      return false;
+      });
+}
